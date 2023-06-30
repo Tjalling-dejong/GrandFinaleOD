a54b796b62393cf77c07a014be7599b8d1d60a13
Wouter Klessens <w.klessens@thuisvester.nl>
2023-06-30 13:11:16 +0200

    Filtered afstanden.sql

--- SQL/Afstanden.sql
+++ SQL/Afstanden.sql
@@ -2,6 +2,9 @@ SELECT
 nic, 
 bedrijf, 
   Geometry::Point([x_Coordinaat], [y_Coordinaat], 28992) as centroid_bedrijf,
+  bu_code,
+  bu_naam,
 	  geom.STCentroid() as centroid_buurt,
 	  geom.STCentroid().STDistance(Geometry::Point([x_Coordinaat], [y_Coordinaat], 28992)) AS distance
   FROM [dbo].[CBS_Buurten_2020_Geo], [dbo].[eMJV_AlgemeneGegevens]
+  WHERE gm_naam IN ('Amsterdam', 'Haarlemmermeer', 'Zaanstad', 'Aalsmeer', 'Amstelveen', 'Diemen', 'Ouder-Amstel', 'Uithoorn');
